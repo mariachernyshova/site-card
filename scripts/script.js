669e0ddb4422c9f9762ebc4f55227361fd37c7a2
mariachernyshova <mary.chernishova@mail.ru>
2023-02-26 13:20:52 +0300

    add video bg: use jquery, vide.js

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// когда происходит загрузка страницы
+$(window).on('load', function (){
+    // Vide.js - video background
+    // находим элемент с id header, укаываем путь относительно документа HTML
+    $('#header').vide('./video/cover', {
+        bgColor: '#242f35'
+    });
+})
+
+
+let header = document.querySelector()
